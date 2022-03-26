4d8c576fc0e31699b1c523daffd6f145f22ed017
JohanTorosjan <johan.torosjan@etu.umontpellier.fr>
2022-03-26 15:44:29 +0100

    blocking routes

--- src/router/index.js
+++ src/router/index.js
@@ -30,45 +30,52 @@ const routes = [
   {
     path: '/CookerSignup',
     name: 'CookerSignup',
-    component: () => import( '../views/CookerSignup.vue')
+    component: () => import( '../views/CookerSignup.vue'),
   },
   {
 
   path: '/customerHome',
   name: 'customerHome',
-  component : () => import('../views/customerHome.vue')
+  component : () => import('../views/customerHome.vue'),
+  meta:{requireAuth:true}
   },
 
   {
     path: '/cookerHome',
     name: 'cookerHome',
-    component : () => import('../views/cookerHome.vue')
+    component : () => import('../views/cookerHome.vue'),
+    meta:{requireAuth:true}
   },
   {
     path: '/AvailablesCook',
     name: 'AvailablesCook',
-    component : () => import('../views/AvailablesCook.vue')
+    component : () => import('../views/AvailablesCook.vue'),
+    meta:{requireAuth:true}
   },
   {
     path: '/MyOrders',
     name: 'MyOrders',
-    component : () => import('../views/MyOrders.vue')
+    component : () => import('../views/MyOrders.vue'),
+    meta:{requireAuth:true}
   },
   {
     path: '/askCooker',
     name: 'askCooker',
-    component : () => import('../views/askCooker.vue')
+    component : () => import('../views/askCooker.vue'),
+    meta:{requireAuth:true}
   },
   {
   path: '/addReceipes',
   name: 'addReceipes',
-  component : () => import('../views/addReceipes.vue')
+  component : () => import('../views/addReceipes.vue'),
+  meta:{requireAuth:true}
   },
   
   {
     path: '/commandTab',
     name: 'commandTab',
-    component : () => import('../views/commandTab.vue')
+    component : () => import('../views/commandTab.vue'),
+    meta:{requireAuth:true}
     },
     
   
@@ -82,5 +89,19 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((to,from,next) => {
+  if(to.meta.requireAuth){
+    if(!sessionStorage.getItem("Token")){
+      next("/");
+    }
+    else{
+      next();
+    }
+  }
+  else{
+    next();
+  }
+});
 
 export default router
